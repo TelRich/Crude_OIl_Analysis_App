69744fffa29b7d3621eca752fb2c30462c31d5cf
TelRich <okorogoodrich@gmail.com>
2023-02-26 22:32:35 +0100

    implement expander

--- pages/Yearly_Analysis.py
+++ pages/Yearly_Analysis.py
@@ -38,111 +38,115 @@ side_display()
 month_order = ['February', 'April', 'June', 'August', 'October', 'November', 'December']
 month_short = [ 'Feb', 'Apr', 'Jun', 'Aug', 'Oct', 'Nov', 'Dec']
 
-st.subheader('Yearly Statistics Summary of Crude Oil')
-if st.checkbox('Show raw data', key='price'):
-    st.subheader('Price by Year')
-    st.write(data)
-    
-# Plotting Data
-def add_line(price, color):
-    fig.add_trace(go.Scatter(x=data.Year, y=data[price],
-                            name=price, line=dict(color=color, 
-                                                    width=3, dash='dot')))
-fig = go.Figure()
-add_line('Min_Price', 'firebrick')
-add_line('Avg_Price', 'royalblue')
-add_line('Max_Price', 'forestgreen')
+st.markdown("<h3> <center> Yearly Statistics Summary of Crude Oil</center></h1>", True)
 
-fig.update_layout(title='Summary of crude oil price by year', width=950,
-                  yaxis_title = 'Price (US$/Barrel)',
-                   legend = dict(
-                    orientation='h',
-                    xanchor="right",
-                    x=0.75,
-                    font = dict(
-                      family="Courier",
-                      size=12,
-                    #   color='white'
+with st.expander('Summary of Crude Oil Price by Year', True):
+  if st.checkbox('Show raw data', key='price'):
+      st.subheader('Price by Year')
+      st.write(data)
+      
+  # Plotting Data
+  def add_line(price, color):
+      fig.add_trace(go.Scatter(x=data.Year, y=data[price],
+                              name=price, line=dict(color=color, 
+                                                      width=3, dash='dot')))
+  fig = go.Figure()
+  add_line('Min_Price', 'firebrick')
+  add_line('Avg_Price', 'royalblue')
+  add_line('Max_Price', 'forestgreen')
+
+  fig.update_layout(title='Summary of crude oil price by year', width=950,
+                    yaxis_title = 'Price (US$/Barrel)',
+                    legend = dict(
+                      orientation='h',
+                      xanchor="right",
+                      x=0.75,
+                      font = dict(
+                        family="Courier",
+                        size=12,
+                      #   color='white'
+                      ),
+                      # bgcolor='olive',
+                      bordercolor='blue',
+                      borderwidth=.5
+                    )
+                    )
+  st.plotly_chart(fig, use_container_width=True) 
+
+
+with st.expander('Summary of Crude Oil Production by Year'):
+  if st.checkbox('Show raw data', key='prod'):
+      st.subheader('Production by Year')
+      st.write(data1)
+      
+  # Plotting Data
+  def add_line(price, color):
+      fig.add_trace(go.Scatter(x=data1.Year, y=data1[price],
+                              name=price, line=dict(color=color, 
+                                                              width=3, dash='dot')))
+  fig = go.Figure()
+  add_line('Min_Production', 'firebrick')
+  add_line('Avg_Production', 'royalblue')
+  add_line('Max_Production', 'forestgreen')
+
+  fig.update_layout(title='Summary of crude oil production by year', width=950,
+                    yaxis_title = 'Production (mbd)',
+                    legend = dict(
+                      orientation='h',
+                      xanchor="right",
+                      x=0.82,
+                      font = dict(
+                        family="Courier",
+                        size=12,
+                      #   color='white'
+                      ),
+                      # bgcolor='olive',
+                      bordercolor='blue',
+                      borderwidth=.5
+                    )
+                    )
+  st.plotly_chart(fig, True)
+
+
+with st.expander('Comparing Year 2021 Crude Oil Production with Year 2022'):
+  if st.checkbox('Show raw data', key='2021vs2022'):
+    st.subheader('Comparing 2021 Crude Oil Production with Year 2022')
+    st.write(data2)
+
+  # Visualizing data
+  def add_line(total_prod, color):
+      ex = total_prod
+      name = ex[-4:] + ' Production'
+      fig.add_trace(go.Scatter(x=data2.month, y=data2[total_prod],
+                              name=name, mode='lines+markers+text',
+                              text=data2[total_prod], textposition='bottom left',
+                              line=dict(color=color, 
+                                          width=3, dash='dot'),
+                              textfont=dict(size=10)))
+  fig = go.Figure()
+  add_line('total_prod_2021', 'royalblue')
+  add_line('total_prod_2022', 'firebrick')
+
+
+  fig.update_layout(title='Comparing year 2021 crude oil production with year 2022', width=950,
+                    yaxis_title = 'Production (mbd)',
+                    xaxis= dict(
+                        tickvals= month_order,
+                        ticktext= month_short
                     ),
-                    # bgcolor='olive',
-                    bordercolor='blue',
-                    borderwidth=.5
-                  )
-                  )
-st.plotly_chart(fig, use_container_width=True) 
-
-
-
-if st.checkbox('Show raw data', key='prod'):
-    st.subheader('Production by Year')
-    st.write(data1)
-    
-# Plotting Data
-def add_line(price, color):
-    fig.add_trace(go.Scatter(x=data1.Year, y=data1[price],
-                            name=price, line=dict(color=color, 
-                                                            width=3, dash='dot')))
-fig = go.Figure()
-add_line('Min_Production', 'firebrick')
-add_line('Avg_Production', 'royalblue')
-add_line('Max_Production', 'forestgreen')
-
-fig.update_layout(title='Summary of crude oil production by year', width=950,
-                  yaxis_title = 'Production (mbd)',
-                  legend = dict(
-                    orientation='h',
-                    xanchor="right",
-                    x=0.82,
-                    font = dict(
-                      family="Courier",
-                      size=12,
-                    #   color='white'
-                    ),
-                    # bgcolor='olive',
-                    bordercolor='blue',
-                    borderwidth=.5
-                  )
-                  )
-st.plotly_chart(fig, True)
-
-if st.checkbox('Show raw data', key='2021vs2022'):
-  st.subheader('Comparing 2021 Crude Oil Production with 2022')
-  st.write(data2)
-
-# Visualizing data
-def add_line(total_prod, color):
-    ex = total_prod
-    name = ex[-4:] + ' Production'
-    fig.add_trace(go.Scatter(x=data2.month, y=data2[total_prod],
-                            name=name, mode='lines+markers+text',
-                            text=data2[total_prod], textposition='bottom left',
-                            line=dict(color=color, 
-                                        width=3, dash='dot'),
-                            textfont=dict(size=10)))
-fig = go.Figure()
-add_line('total_prod_2021', 'royalblue')
-add_line('total_prod_2022', 'firebrick')
-
-
-fig.update_layout(title='Comparing year 2021 crude oil production with that of 2022', width=950,
-                  yaxis_title = 'Production (mbd)',
-                  xaxis= dict(
-                      tickvals= month_order,
-                      ticktext= month_short
-                  ),
-                  legend = dict(
-                    orientation='h',
-                    xanchor="right",
-                    x=0.82,
-                    font = dict(
-                      family="Courier",
-                      size=12,
-                    #   color='white'
-                    ),
-                    # bgcolor='olive',
-                    bordercolor='blue',
-                    borderwidth=.5
-                  )
-                  )
-fig.update_yaxes(showticklabels=False)
-st.plotly_chart(fig, True)
+                    legend = dict(
+                      orientation='h',
+                      xanchor="right",
+                      x=0.82,
+                      font = dict(
+                        family="Courier",
+                        size=12,
+                      #   color='white'
+                      ),
+                      # bgcolor='olive',
+                      bordercolor='blue',
+                      borderwidth=.5
+                    )
+                    )
+  fig.update_yaxes(showticklabels=False)
+  st.plotly_chart(fig, True)
